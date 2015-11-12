01a1186142fc163ebe13a5580d9464f93d0f6543
TheNotary <no@email.plz>
2015-11-12 10:28:00 -0800

    whitespace: shrunk down length of routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   end
 
   resources :posts_themes
-
   resources :home_fields
 
   resources :quotes do
@@ -20,20 +19,15 @@ Rails.application.routes.draw do
   get "/submit-report/:id" => "social_media_platforms#submit_report_show", :as => "submit_report_show"
 
   resources :subscribes
-
   resources :people
 
   resources :contacts , except: :index
   get "/contacts" => "contacts#new"
 
   resources :contacts_topics
-
   resources :pages_questions
-
   resources :question_user_submissions
-
   resources :uploaded_files
-
   resources :further_reads
 
   get "resources/further-readings" => "further_reads#index"
@@ -43,21 +37,13 @@ Rails.application.routes.draw do
   get "about/faqs" => "faqs#index"
 
   resources :content_management_pages
-
   resources :question_answers
-
   resources :question_options
-
   resources :languages
-
   resources :countries
-
   resources :question_users
-
   resources :questions
-
   resources :pages
-
   resources :social_media_platforms
 
   resources :posts, path: 'news-and-analysis' do
@@ -121,60 +107,4 @@ Rails.application.routes.draw do
   match "/500" =>  "home#error", via: [ :get, :post, :patch, :delete ]
   match "/422" =>  "home#error", via: [ :get, :post, :patch, :delete ]
   match "*path" => "home#error", via: [ :get, :post, :patch, :delete ]
-
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
