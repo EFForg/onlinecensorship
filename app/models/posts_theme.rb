f2e470e8ee8ba5da1ad139bbb685eddd663a2f9a
ASTM <astm_desig@hotmail.com>
2016-02-10 15:15:03 +0200

    fixed the themes statistics issue

--- app/models/posts_theme.rb
+++ app/models/posts_theme.rb
@@ -1,12 +1,13 @@
 # PostsTheme
 #
-# The admin can add theme for every post 
+# The admin can add theme for every post
 #
 class PostsTheme < ActiveRecord::Base
 
   ## DB relations
   has_many :posts
   has_many :quotes
+  has_many :posts_posts_themes
 
   # For use search method
   extend Search
