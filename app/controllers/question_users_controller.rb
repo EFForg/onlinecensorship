4d84532fb8f9c24a0fbba2c8cd26b6c01d2b99b7
ASTM <astm_desig@hotmail.com>
2016-02-16 16:17:05 +0200

    export the platform excel sheet

--- app/controllers/question_users_controller.rb
+++ app/controllers/question_users_controller.rb
@@ -13,7 +13,68 @@ class QuestionUsersController < ApplicationController
       }
       # Using it to export excel file
       format.xls {
-        @question_users = QuestionUser.all
+
+        # platform = SocialMediaPlatform.where(:id=>params[:platform]).first
+        # @questions = []
+        # if platform.pages.present?
+        #   platform.pages.order(:theorder => :asc).each do |page|
+        #     if page.pages_questions.present?
+        #       # get the questions
+        #       page.pages_questions.each do |page_question|
+        #         if page_question.question.present?
+        #           #  the questions
+        #           @questions << page_question.question.question_title.gsub('{platform}',platform.title)
+        #
+        #         end
+        #       end
+        #
+        #       # get the answers
+        #       if page.question_user_submissions.present?
+        #
+        #       end
+        #
+        #     end
+        #   end
+        # end
+
+        # @platform = SocialMediaPlatform.where(:id=>params[:platform]).first
+        # @questions = []
+        # @question_users = QuestionUser.all
+        # @question_users.each do |question_user|
+        #   if question_user.question_user_submissions.present?
+        #     # Get the user submission
+        #     question_user.question_user_submissions.each do |submission|
+        #       # Get the same platform pages
+        #       if submission.page.present? && submission.page.social_media_platform.id == @platform.id
+        #         #  the questions
+        #         question = submission.question_answer.question.question_title.gsub('{platform}',submission.page.social_media_platform.title)
+        #         @questions << question unless @questions.include?(question)
+        #       end
+        #     end
+        #   end
+        # end
+
+        @questions = []
+        @question_users = []
+        @platform = SocialMediaPlatform.where(:id=>params[:platform]).first
+        # Get the platform users
+        QuestionUser.all.each do |question_user|
+          if question_user.question_user_submissions.present?
+            question_user.question_user_submissions.each do |submission|
+              if submission.page.present? && submission.page.social_media_platform.id == @platform.id
+                @question_users << question_user unless @question_users.include?(question_user)
+              end
+            end
+          end
+        end
+
+        @question_users.each do |question_user|
+          question_user.question_user_submissions.each do |submission|
+            #  the questions
+            question = submission.question_answer.question.question_title.gsub('{platform}',submission.page.social_media_platform.title)
+            @questions << question unless @questions.include?(question)
+          end
+        end
       }
     end
   end
