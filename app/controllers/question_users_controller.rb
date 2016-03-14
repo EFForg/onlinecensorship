ee83f756f45e4aa0ab342e9477a4226375b4fa48
ASTM <astm_desig@hotmail.com>
2016-03-14 13:58:28 +0200

    delete the old export code

--- app/controllers/question_users_controller.rb
+++ app/controllers/question_users_controller.rb
@@ -42,35 +42,6 @@ class QuestionUsersController < ApplicationController
             end
           end
         end
-
-        # binding.pry
-
-
-        # QuestionUser.all.each do |question_user|
-        #   if question_user.question_user_submissions.present?
-        #     question_user.question_user_submissions.each do |submission|
-        #       if submission.page.present? && submission.page.social_media_platform.id == @platform.id
-        #         @question_users << question_user unless @question_users.include?(question_user)
-        #       end
-        #     end
-        #   end
-        # end
-
-        # @question_users.each do |question_user|
-        #   question_user.question_user_submissions.each do |submission|
-        #     if submission.page.present?
-        #       platfrom=submission.page.social_media_platform.title
-        #     else
-        #       platfrom="platfrom"
-        #     end
-        #
-        #     if submission.question_answer.present? && submission.question_answer.question.present?
-        #       #  the questions
-        #       current_question = submission.question_answer.question.question_title.gsub('{platform}',platfrom)
-        #       @questions << current_question unless @questions.include?(current_question)
-        #     end
-        #   end
-        # end
       }
     end
   end
