5af5c552d642feebb90d3374f8a35f5df5c66dfa
Morad Taleeb <morad.taleeb@gmail.com>
2016-08-25 11:19:27 +0300

    Sentry DSN

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,6 +2,6 @@ require 'raven'
 
 unless ENV['sentry_dsn'].nil?
   Raven.configure do |config|
-    config.dsn = ENV['sentry_dsn']
+    config.dsn = "https://6e9a4086fc244ffa92ddefc5a6e8f7ce:2b4179645a6640a3a3df76a23276e60d@sentry.eff.org/5" # ENV['sentry_dsn']
   end
 end
