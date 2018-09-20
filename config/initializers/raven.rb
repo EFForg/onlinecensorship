38ad4a4ee3a12cd8ecc52d571e37e454909d6249
Syd Young <sky@eff.org>
2018-09-19 18:57:19 -0700

    Use SENTRY_DSN env variable

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,7 +1,7 @@
 require 'raven'
 
-unless ENV['sentry_dsn'].nil?
+unless ENV['SENTRY_DSN'].nil?
   Raven.configure do |config|
-    config.dsn = "https://6e9a4086fc244ffa92ddefc5a6e8f7ce:2b4179645a6640a3a3df76a23276e60d@sentry.eff.org/5" # ENV['sentry_dsn']
+    config.dsn = ENV['SENTRY_DSN']
   end
 end
