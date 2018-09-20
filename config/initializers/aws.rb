e319ee599b8ffd64a05580d91e775cc656ffecb2
Syd Young <sky@eff.org>
2018-09-20 14:19:01 -0700

    Make all ENV variables uppercase

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,3 @@
 # the AWS gem will look for these configs as capitalized
-ENV['AWS_ACCESS_KEY_ID'] = ENV['aws_access_key_id']
-ENV['AWS_SECRET_ACCESS_KEY'] = ENV['aws_secret_access_key']
+ENV['AWS_ACCESS_KEY_ID'] = ENV['AWS_ACCESS_KEY_ID']
+ENV['AWS_SECRET_ACCESS_KEY'] = ENV['AWS_SECRET_ACCESS_KEY']
