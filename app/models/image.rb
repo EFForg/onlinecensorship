3b20cdb09aac6f349ee7ded288d342dff25b1e13
Syd Young <sky@eff.org>
2018-09-21 15:55:58 -0700

    S3 fix for takedown images

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,7 @@ class Image < ApplicationRecord
   belongs_to :takedown
   has_attached_file :file, path: '/:class/:attachment/:id_partition/:style'\
                                  '/:filename',
-                           url: '/:class/:attachment/:id_partition/:style'\
-                                 '/:filename'
+                           url: ':s3_domain_url'
   validates_attachment :file,
       content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
