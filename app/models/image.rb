37b92d248090dcb48005e4dac7507940c71ae939
Syd Young <sky@eff.org>
2018-09-21 14:05:38 -0700

    Update paperclip S3 config

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,8 @@
 class Image < ApplicationRecord
   belongs_to :takedown
   has_attached_file :file, path: '/:class/:attachment/:id_partition/:style'\
+                                 '/:filename',
+                           url: '/:class/:attachment/:id_partition/:style'\
                                  '/:filename'
   validates_attachment :file,
       content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
