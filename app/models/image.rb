e536574a3d3d6ecc5b29a6f2e398ae0486cad77a
Syd Young <sky@eff.org>
2018-09-21 13:51:11 -0700

    Move most images to pub/ directory

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
 class Image < ApplicationRecord
   belongs_to :takedown
-  has_attached_file :file
+  has_attached_file :file, path: '/:class/:attachment/:id_partition/:style'\
+                                 '/:filename'
   validates_attachment :file,
       content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -16,3 +16,6 @@ end
 Paperclip.interpolates :parent_id do |a, s|
   a.instance.contract.id
 end
+
+Paperclip::Attachment.default_options[:path] = '/pub/:class/:attachment/'\
+                                               ':id_partition/:style/:filename'
