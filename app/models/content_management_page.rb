7e4853cbf6ae03f686a1de34c188059e4db14315
Syd Young <sky@eff.org>
2018-12-17 17:57:55 -0800

    Add styling callbacks to appeal pages
    Resolves #135

--- app/models/content_management_page.rb
+++ app/models/content_management_page.rb
@@ -14,6 +14,8 @@ class ContentManagementPage < ApplicationRecord
   # abort destruction of hard-coded pages that wil break the site
   before_destroy :prevent, if: ->(p) { HARD_CODED_PAGES.include? p.page }
 
+  before_save :style, if: ->(p) { p.appeal_page }
+
   translates :content, :brief, :page_title
   attribute :content
   attribute :brief
@@ -23,5 +25,14 @@ class ContentManagementPage < ApplicationRecord
 
   def prevent
     throw :abort
+  end
+
+  def style
+    c = content.gsub(/<\/?div[^>]*>/, '').split("<h2>")
+    title = c.first == '' ? c.shift(2).join("<h2>") : ''
+    c = c.map do |question|
+      '<div class="appeal-question"><h2>' + question + '</div>'
+    end.join('')
+    self.content = title + c
   end
 end
